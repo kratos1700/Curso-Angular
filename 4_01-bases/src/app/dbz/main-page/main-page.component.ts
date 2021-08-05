7d70c99a4424200be5529fa3e1183112fe8431fd
kratos1700 <hunterstarsa@hotmail.com>
2021-08-05 20:33:11 +0200

    uso del [(ngModel)]

--- 4_01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 4_01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje{
+  nombre: string;
+  poder:number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
+
+
+
+  nuevo:Personaje={
+    nombre: 'Trunks',
+    poder: 11000
+  }
+
+
 
   constructor() { 
 
@@ -15,12 +29,22 @@ export class MainPageComponent implements OnInit {
 
   }
 
+
+
+  cambiarNombre(event:any){
+
+    console.log( event.target.value );
+    
+  }
+
   //aqui declaramos las funciones/ metodos
   agregar(): void {
    
-    console.log('Holas!!');
+    console.log(this.nuevo);
     
 
   }
+
+  
 
 }
