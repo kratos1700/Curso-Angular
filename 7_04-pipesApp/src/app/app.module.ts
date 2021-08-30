bf07cfa77366e1aa8f334504ac7ae9561fb43404
kratos1700 <hunterstarsa@hotmail.com>
2021-08-30 10:48:54 +0200

    creacion de PipesApp, importacion de PrimeNg , preparacion del proyecto

---
+++ 7_04-pipesApp/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+//modulo personalizat
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+ 
+  imports: [
+    BrowserModule,
+    PrimeNgModule,
+    SharedModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
