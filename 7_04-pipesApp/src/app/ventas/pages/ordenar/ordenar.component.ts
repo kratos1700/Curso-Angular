bc0a9d032f7e4d37e33a013b89d97a24b83a83fa
kratos1700 <hunterstarsa@hotmail.com>
2021-09-12 11:25:46 +0200

    Creacion de pipes personalizados y prueba de tabla primeng con opcion de ordenacion

--- 7_04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
+++ 7_04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -10,6 +10,7 @@ export class OrdenarComponent implements OnInit {
 
 
   enMayusculas:boolean = true;
+  ordenarPor:string='';
 
 heroes :Heroe[]=[
  { nombre:'Superman',
@@ -48,6 +49,10 @@ heroes :Heroe[]=[
 
     this.enMayusculas= !this.enMayusculas;
 
+  }
+
+  cambiarOrden(valor:string){
+    this.ordenarPor = valor;
   }
 
 }
