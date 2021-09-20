e723b1ea062e022080b3bd85a8c68b6a4db19b29
kratos1700 <hunterstarsa@hotmail.com>
2021-09-20 14:41:33 +0200

    importacion de componentes de material angular y Flex. creacion de un sidenav con opciones a las rutas

--- 8_05-heroesApp/src/app/heroes/heroes.module.ts
+++ 8_05-heroesApp/src/app/heroes/heroes.module.ts
@@ -5,7 +5,12 @@ import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
+
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -19,7 +24,9 @@ import { HeroesRoutingModule } from './heroes-routing.module';
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+    FlexLayoutModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
