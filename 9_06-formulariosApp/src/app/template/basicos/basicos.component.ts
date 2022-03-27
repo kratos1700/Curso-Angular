658d0098eed1b24caaf01c9d1206b953d7ba33cc
Kratos1700 <hunterstarsa@hotmail.com>
2022-03-27 21:30:25 +0200

    validacion de campos mostrando mensage en el html

--- 9_06-formulariosApp/src/app/template/basicos/basicos.component.ts
+++ 9_06-formulariosApp/src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -8,14 +8,30 @@ import { NgForm } from '@angular/forms';
   ]
 })
 export class BasicosComponent implements OnInit {
+  // recuperamos el formulario de la referencia local con el ViewChild
+  @ViewChild('miFormulario') miFormulario!:NgForm
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  guardar(miFormulario:NgForm){
-    console.log(miFormulario.value);
+//funcion para sustituir el codigo del html para optimizarlo
+nombreValido():boolean{
+  return this.miFormulario?.controls.producto?.invalid &&
+ this. miFormulario?.controls.producto?.touched
+
+}
+
+precioValido():boolean{
+return this.miFormulario?.controls.precio?.value < 0 &&
+this. miFormulario?.controls.precio?.touched
+}
+
+
+ // guardar(miFormulario:NgForm){
+  guardar(){
+    console.log(this.miFormulario);
     
   }
 
