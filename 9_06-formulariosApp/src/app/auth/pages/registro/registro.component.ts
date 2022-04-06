a8fff6946541764314ea876d76c9bf386e29e26b
Kratos1700 <hunterstarsa@hotmail.com>
2022-04-06 17:23:11 +0200

    validaciones de patter/regex email

--- 9_06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ 9_06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -8,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  nombreApellidoRegex:string="([a-zA-Z]+) ([a-zA-Z]+)";
+  emailRegex:string="[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"
+
+  //creamos el formulario
+  miFormulario:FormGroup=this.fb.group({
+    nombre:['',[Validators.required, Validators.pattern(this.nombreApellidoRegex)]],
+    email:['',[Validators.required, Validators.pattern(this.emailRegex)]],
+
+  })
+
+  // creamos el FormBuilder
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  // funcion para visualizar un mensage error si
+  //  un campo es invalido
+  campoNoValido(campo:string){
+    return this.miFormulario.get(campo)?.invalid &&
+    this.miFormulario.get(campo)?.touched
+  }
+
+  submitFormulario(){
+    console.log(this.miFormulario.value);
+    // hacemos saltar las validaciones de todos los campos
+    this.miFormulario.markAllAsTouched();
+    
   }
 
 }
