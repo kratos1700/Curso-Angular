dd23877288e67fdf31aaaae7b9a3a0531c613943
Kratos1700 <hunterstarsa@hotmail.com>
2022-04-06 13:06:23 +0200

    añadido dinamico component

--- 9_06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
+++ 9_06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -9,65 +9,65 @@ import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@ang
 })
 export class DinamicosComponent implements OnInit {
 
-  miFormulario:FormGroup= this.fb.group({
-     // para establecer varios validadores se ponen entre []
-     nombre: ['',[Validators.required, Validators.minLength(3)],],
-     // iniciamos con un arreglo vacio el campo favoritos
-     favoritos:this.fb.array([
-       //coleciones de form controls
-       ['Basquet'],
-       ['Futbol']
-     ],Validators.required)
-    
-  })
-// creamos un nuevo control para enlazar el campo
-// favoritos del html
-  nuevoFavorito:FormControl= this.fb.control('',Validators.required)
+  miFormulario: FormGroup = this.fb.group({
+    // para establecer varios validadores se ponen entre []
+    nombre: ['', [Validators.required, Validators.minLength(3)],],
+    // iniciamos con un arreglo vacio el campo favoritos
+    favoritos: this.fb.array([
+      //coleciones de form controls
+      ['Basquet'],
+      ['Futbol']
+    ], Validators.required)
 
-  get favoritosArr(){
+  })
+  // creamos un nuevo control para enlazar el campo
+  // favoritos del html
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required)
+
+  get favoritosArr() {
     return this.miFormulario.get('favoritos') as FormArray;
   }
 
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
 
-   //funcion para validar campos y mostrar mensage de error
-   campoValido(campo:string){
+  //funcion para validar campos y mostrar mensage de error
+  campoValido(campo: string) {
     return this.miFormulario.controls[campo].errors &&
-    this.miFormulario.controls[campo].touched
+      this.miFormulario.controls[campo].touched
   }
 
-//funcion agregar favorito
-agregarFavorito(){
-  //si el campo es invalido no hacemos nada, retornamos
-  if (this.nuevoFavorito.invalid){
-    return;
+  //funcion agregar favorito
+  agregarFavorito() {
+    //si el campo es invalido no hacemos nada, retornamos
+    if (this.nuevoFavorito.invalid) {
+      return;
+    }
+
+    // añadimos el elemento 
+    // this.favoritosArr.push(new FormControl(this.nuevoFavorito.value,Validators.required));
+    this.favoritosArr.push(this.fb.control(this.nuevoFavorito.value, Validators.required))
+
+    //eliminamos el valor del campo
+    this.nuevoFavorito.reset();
   }
 
-  // añadimos el elemento 
- // this.favoritosArr.push(new FormControl(this.nuevoFavorito.value,Validators.required));
-  this.favoritosArr.push(this.fb.control(this.nuevoFavorito.value,Validators.required))
 
-  //eliminamos el valor del campo
-  this.nuevoFavorito.reset();
-}
+  guardar() {
 
-
-  guardar(){
-
-    if(this.miFormulario.invalid){
+    if (this.miFormulario.invalid) {
       // hace que toque todos los campos y salten los errores si hay
-     // this.miFormulario.markAllAsTouched();
+      // this.miFormulario.markAllAsTouched();
       this.miFormulario.markAllAsTouched();
       return;
     }
     console.log(this.miFormulario.value);
     //reseteamos el formulario
     this.miFormulario.reset();
-    
+
   }
 
 }
