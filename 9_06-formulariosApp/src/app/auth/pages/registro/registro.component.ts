98f8427cb272d75f1f1011f280f71ba904c1d9ee
Kratos1700 <hunterstarsa@hotmail.com>
2022-04-09 17:27:33 +0200

    mensages de error personalizados al validar campos

--- 9_06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ 9_06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -13,39 +13,59 @@ import { ValidatorsService } from 'src/app/shared/validators/validators.service'
 export class RegistroComponent implements OnInit {
 
   //creamos el formulario
-  miFormulario:FormGroup=this.fb.group({
-    nombre:['',[Validators.required, Validators.pattern(this.validatorService.nombreApellidoRegex)]],
-    email:['',[Validators.required, Validators.pattern(this.validatorService.emailRegex)],[this.emailValidator]],
-    username:['',[Validators.required,this.validatorService.noPuedeSerStrider]],
-    password:['',[Validators.required,Validators.minLength(6)]],
-    password2:['',[Validators.required] ],
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.pattern(this.validatorService.nombreApellidoRegex)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailRegex)], [this.emailValidator]],
+    username: ['', [Validators.required, this.validatorService.noPuedeSerStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
 
     // ponemos , y abrimos llaves para mandor opciones al form group
   }, {
-    validators:[this.validatorService.camposIguales('password','password2')]
+    validators: [this.validatorService.camposIguales('password', 'password2')]
 
   })
 
+  //emailErrorMsg: string='';
+
+  // get para capturar los errores y
+  // evaluarlos dando una respuesta en funcion del tipo de dato requerido
+  get emailErrorMsg(): string {
+
+    const errors = this.miFormulario.get('email')?.errors;
+    if (errors?.required) {
+      return 'El Email es obligatorio'
+    } else if (errors?.pattern) {
+      return 'El formato de Email es incorrecto'
+    } else if (errors?.emailUsado) {
+      return 'El Email ya esta en uso'
+    }
+    return ''
+  }
+
   // creamos el FormBuilder, creamos el servicio de validaciones personalizadas
-  constructor(private fb:FormBuilder, 
-    private validatorService:ValidatorsService,
-    private emailValidator:EmailValidatorService) { }
+  constructor(private fb: FormBuilder,
+    private validatorService: ValidatorsService,
+    private emailValidator: EmailValidatorService) { }
 
   ngOnInit(): void {
   }
 
   // funcion para visualizar un mensage error si
   //  un campo es invalido
-  campoNoValido(campo:string){
+  campoNoValido(campo: string) {
     return this.miFormulario.get(campo)?.invalid &&
-    this.miFormulario.get(campo)?.touched
+      this.miFormulario.get(campo)?.touched
   }
 
-  submitFormulario(){
+
+  submitFormulario() {
     console.log(this.miFormulario.value);
     // hacemos saltar las validaciones de todos los campos
     this.miFormulario.markAllAsTouched();
-    
+
   }
+
+
 
 }
--- 9_06-formulariosApp/src/app/shared/validators/email-validator.service.ts
+++ 9_06-formulariosApp/src/app/shared/validators/email-validator.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -21,6 +21,8 @@ export class EmailValidatorService implements AsyncValidator{
     return this.http.get<any[]>(`http://localhost:3000/usuarios?q=${email}`)
     // le pasamos por el pipe para devolver lo que queremos
     .pipe( 
+      //usamos el delay para simular una peticion al backend
+      delay(3000),
       map(res=>{
         //Si la respuesta es igual a 0 podemos usar el correo, es null
         // sino ese correo esta usado al devolver algun dato
