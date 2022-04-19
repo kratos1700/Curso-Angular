fd4be2092ae57386c9e2fe33f57a19d04152efa9
Kratos1700 <hunterstarsa@hotmail.com>
2022-04-19 22:07:52 +0200

    configurando los componentes, rutas y servicios

--- 10_07-selectores/src/app/app-routing.module.ts
+++ 10_07-selectores/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+// carga lazy
+
+{
+  path: 'selector',
+loadChildren: ()=> import ('./paises/paises.module').then( m =>m.PaisesModule)
+
+},
+// cualquier cosa que escribamos al url redirigira a selector
+{
+  path:'**', redirectTo:'selector'
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
