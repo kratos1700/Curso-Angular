19ce4332d6183a18125e3c884b9df6cf08784a7f
Kratos1700 <hunterstarsa@hotmail.com>
2022-04-21 13:30:40 +0200

    implementacion de las peticiones http , obtencion de los paises y fronteras

--- 10_07-selectores/src/app/paises/pages/selector-page/selector-page.component.ts
+++ 10_07-selectores/src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {switchMap, tap} from 'rxjs/operators'
+import { PaisSmall } from '../../interfaces/paises.interface';
+import { PaisesService } from '../../services/paises.service';
 
 @Component({
   selector: 'app-selector-page',
@@ -7,9 +11,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectorPageComponent implements OnInit {
 
-  constructor() { }
+  // creamos el formulario para trabajar con form reactivos
+  miFormulario :FormGroup= this.fb.group({
+
+    region:['', Validators.required],
+    pais:['', Validators.required],
+    frontera:['', Validators.required]
+
+  })
+
+  //llenar selector
+  regiones:string[]=[];
+  paises:PaisSmall[]=[];
+  fronteras:string[]=[];
+
+  constructor(private fb:FormBuilder,private paisesService:PaisesService) { }
 
   ngOnInit(): void {
+
+    //cargamos las regiones
+    this.regiones=this.paisesService.regiones;
+
+    this.miFormulario.get('region')?.valueChanges
+    .pipe(
+      //lanzamos un efecto secundario con tap para resetear los campos
+      //una vez cambiada la region. (_) es para decir k no importa el valor que tenemos
+      tap( (_) =>{
+        this.miFormulario.get('pais')?.reset('');
+      }),
+       //usamos el pipe para modificar el valor 
+      // una vez que obtenemos el valor, lo cambiamos por la siguiente peticion
+      switchMap(region => this.paisesService.getPaisesPorRegion(region)) 
+    )
+    .subscribe(paises =>{
+      this.paises=paises    
+    })
+
+
+    // cuando el pais
+    this.miFormulario.get('pais')?.valueChanges
+    .pipe(
+      // limpiamos las fornteras
+      tap( (_)=> { this.fronteras=[];
+      this.miFormulario.get('frontera')?.reset('')}),
+      switchMap(codigo => this.paisesService.getPaisesPorCodigo(codigo))
+    )
+    .subscribe( pais => {
+      // rellenamos las fronteras pero si el pais no tiene lo rellenamos con un arreglo vacio
+      this.fronteras= pais?.borders || [];
+      
+    })
+
   }
 
+
+  guardar(){
+    console.log("guardando");
+    
+  }
 }
--- 10_07-selectores/src/app/paises/services/paises.service.ts
+++ 10_07-selectores/src/app/paises/services/paises.service.ts
@@ -1,9 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Pais, PaisSmall } from '../interfaces/paises.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
 
-  constructor() { }
+
+  private _regiones: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  //variable de la url para la peticion http
+  private baseUrl: string = 'https://restcountries.com/v2'
+  // getter para obtener las _regiones
+  get regiones(): string[] {
+    //desestructuramos para no modificar la privada 
+    return [...this._regiones];
+  }
+
+
+  constructor(private http: HttpClient) { }
+
+
+
+  //funcion para recibir la peticion de la region y obtener los paises, devuelve un observable de paises
+  getPaisesPorRegion(region: string): Observable<PaisSmall[]> {
+
+    const url: string = `${this.baseUrl}/region/${region}?fields=name,alpha3Code`
+    // retornamos un observable de un arreglo de paises
+    return this.http.get<PaisSmall[]>(url);
+  }
+
+  //funcion para obtener paises por codigo, el observable emite un pais o null
+  getPaisesPorCodigo(codigo: string): Observable<Pais | null> {
+   // si no es un codigo valido
+    if (!codigo) {
+      //of nos permite devolver un observable
+      return of(null)
+    }
+
+    const url: string = `${this.baseUrl}/alpha/${codigo}`;
+    return this.http.get<Pais>(url)
+
+  }
 }
