dcbdd1253e7d2ffb0b36e6e2c93b2ea2cf74607d
Kratos1700 <hunterstarsa@hotmail.com>
2022-04-24 16:55:28 +0200

    creacion del menuItem con sus rutas

--- 12_09-mapasApp/src/app/shared/menu/menu.component.ts
+++ 12_09-mapasApp/src/app/shared/menu/menu.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+//creamos una interface para las opciones/rutas del menu
+interface MenuItem {
+  ruta: string,
+  nombre: string
+}
 
 @Component({
   selector: 'app-menu',
@@ -6,6 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+
+  // opciones a mostrar del menu con las rutas
+  menuItems: MenuItem[] = [
+
+    {
+      ruta: 'mapas/fullscreen',
+      nombre: 'FullScreen'
+    },
+    {
+      ruta: 'mapas/zoom-range',
+      nombre: 'Zoom Range'
+    },
+    {
+      ruta: 'mapas/marcadores',
+      nombre: 'Marcadores'
+    },
+    {
+      ruta: 'mapas/propiedades',
+      nombre: 'Propiedades'
+    }
+  ];
 
   constructor() { }
 
