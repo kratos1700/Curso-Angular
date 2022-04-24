7ff23dcff0de6e7329760415305e36523369804f
Kratos1700 <hunterstarsa@hotmail.com>
2022-04-24 18:19:45 +0200

    .

--- 12_09-mapasApp/src/environments/environment.prod.ts
+++ 12_09-mapasApp/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
   mapboxToken:'pk.eyJ1Ijoia3JhdG9zMTcwMCIsImEiOiJjbDJkZDFpdDkwcXBsM2pxcmZoOW42OHI5In0.C4vus8I7O1HSvQvazHqT0w'
-
 };
--- 12_09-mapasApp/src/environments/environment.ts
+++ 12_09-mapasApp/src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   mapboxToken:'pk.eyJ1Ijoia3JhdG9zMTcwMCIsImEiOiJjbDJkZDFpdDkwcXBsM2pxcmZoOW42OHI5In0.C4vus8I7O1HSvQvazHqT0w'
+
 };
 
 /*
