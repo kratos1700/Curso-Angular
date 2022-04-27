179ab589ea7b87626a322611c96000726bc1d7aa
Kratos1700 <hunterstarsa@hotmail.com>
2022-04-27 19:12:53 +0200

    implementacion de las propiedades para mostrar minimapas

--- 12_09-mapasApp/src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ 12_09-mapasApp/src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,13 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component,ElementRef,Input, OnInit, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
   styleUrls: ['./mini-mapa.component.css']
 })
-export class MiniMapaComponent implements OnInit {
+export class MiniMapaComponent implements AfterViewInit {
+
+//recibimos la lng i lat
+@Input() lngLat: [number,number]= [0,0];
+
+// con la ref del mapa #mapa
+@ViewChild('mapa') divMapa!:ElementRef;
 
   constructor() { }
+
+  ngAfterViewInit(): void {
+
+    const map = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      // Longitud, latitud  en mapbox, en google maps es diferente lat , lng 
+      center:this.lngLat,
+      zoom:14.5,
+      interactive:false
+    });
+
+    new mapboxgl.Marker()
+    .setLngLat(this.lngLat)
+    .addTo(map);
+
+   
+  }
 
   ngOnInit(): void {
   }
