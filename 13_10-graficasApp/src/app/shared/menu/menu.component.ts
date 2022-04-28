b903d0f480f570e6b99a47d804d4204111a25924
Kratos1700 <hunterstarsa@hotmail.com>
2022-04-28 18:44:54 +0200

    creacion del menu con las rutas

--- 13_10-graficasApp/src/app/shared/menu/menu.component.ts
+++ 13_10-graficasApp/src/app/shared/menu/menu.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+//interface para las rutas del menu
+interface MenuItem{
+  ruta:string,
+  texto:string
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -8,6 +14,14 @@ import { Component, OnInit } from '@angular/core';
 export class MenuComponent implements OnInit {
 
   constructor() { }
+
+// opciones del menu
+  menu:MenuItem[]=[
+    {ruta:'/graficas/barra', texto:'Barras'},
+    {ruta:'/graficas/barra-doble', texto:'Barras Dobles'},
+    {ruta:'/graficas/dona', texto:'Dona'},
+    {ruta:'/graficas/dona-http', texto:'Dona Http'},
+  ]
 
   ngOnInit(): void {
   }
