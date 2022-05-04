f3498c7d84621edd14e9c7dda510d3ae81f6f9c3
Kratos1700 <hunterstarsa@hotmail.com>
2022-05-04 21:14:00 +0200

    cambiando los colores de las barras

--- 13_10-graficasApp/src/app/graficas/pages/barras/barras.component.ts
+++ 13_10-graficasApp/src/app/graficas/pages/barras/barras.component.ts
@@ -45,9 +45,9 @@ export class BarrasComponent implements OnInit {
   public barChartData: ChartData<'bar'> = {
     labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
     datasets: [
-      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' },
-      { data: [ 12, 22, 45, 98, 23, 87, 69 ], label: 'Series C' }
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A',backgroundColor:'#6DFCAA', hoverBackgroundColor:'red' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' , backgroundColor:'#C2D95D',hoverBackgroundColor:'red'},
+      { data: [ 12, 22, 45, 98, 23, 87, 69 ], label: 'Series C', backgroundColor:'#F0C673' ,hoverBackgroundColor:'red'}
     ]
   };
 
