ad0060c2e4452ed95c6b940804f8af0397df5cc2
Kratos1700 <hunterstarsa@hotmail.com>
2022-05-21 19:54:17 +0200

    inicio del proyecto pra el frond, creacion de las rutas, implementacion de las rutas, importacion de estilos i pantalla html login y registro

---
+++ 15_12-authApp/src/app/auth/pages/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  miFormulario:FormGroup= this.fb.group({
+    email:['',[Validators.required,Validators.email]],
+    password:['',[Validators.required,Validators.minLength(6)]],
+  })
+
+  constructor(private fb:FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    console.log(this.miFormulario.value);
+    console.log(this.miFormulario.valid);
+    
+  }
+
+}
