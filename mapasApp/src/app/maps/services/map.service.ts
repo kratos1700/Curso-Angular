71967f425c3ef2d7b10f9d08ce67fc856af11bdc
Kratos1700 <hunterstarsa@hotmail.com>
2022-05-24 19:47:46 +0200

    añadidos los marcadores de las busquedas

--- mapasApp/src/app/maps/services/map.service.ts
+++ mapasApp/src/app/maps/services/map.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { LngLatLike, Map } from 'mapbox-gl';
+import { LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
+import { Feature } from '../interfaces/places.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,9 @@ import { LngLatLike, Map } from 'mapbox-gl';
 export class MapService {
 
   private map?:Map;
+
+  private markers:Marker[]=[];
+
 
 /**
  * GET Y SET PARA MAPA
@@ -28,6 +32,33 @@ export class MapService {
       center:coords
     })
 
+  }
+
+
+  createMarkersFromPlaces(places:Feature[]){
+    if (!this.map) throw Error('Mapa no inicializado');
+// borramos los marcadores
+    this.markers.forEach(marker=>marker.remove());
+    const newMarkers=[];
+
+    for(const place of places){
+      const[lng, lat]= place.center;
+      // creamos el popup
+      const popup= new Popup()
+      .setHTML(`
+      <h6>${place.text}</h6>
+      <span>${place.place_name}</span>
+      `);
+      // creamos una marca y le añadimos la lng y lat
+      const newMarker=new Marker()
+      .setLngLat([lng,lat])
+      .setPopup(popup)
+      .addTo(this.map);
+
+      newMarkers.push(newMarker);
+    }
+
+    this.markers=newMarkers;
   }
 
   constructor() { }
--- mapasApp/src/app/maps/services/places.service.ts
+++ mapasApp/src/app/maps/services/places.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { PlacesApiClient } from '../api/places.Api.Client';
 import { Feature, PlacesResponse } from '../interfaces/places.interfaces';
+import { MapService } from './map.service';
 
 @Injectable({
   providedIn: 'root'
@@ -21,7 +22,9 @@ export class PlacesService {
 
 
 
-  constructor(private placesApi:PlacesApiClient) {
+  constructor(private placesApi:PlacesApiClient,
+    private mapService:MapService) {
+
     this.getUserLocation();
    }
 
@@ -59,8 +62,7 @@ export class PlacesService {
     if(!this.useLocation)throw Error('No hay userLocation');
     
     this.isLoadingPlaces=true;
-    //  this.http.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?proximity=0.8001659559107281%2C40.88227691341305&types=place%2Cpostcode%2Caddress&language=es&access_token=YOUR_MAPBOX_ACCESS_TOKEN`)
-     this.placesApi.get<PlacesResponse>(`/${query}.json`,{
+      this.placesApi.get<PlacesResponse>(`/${query}.json`,{
        params:{
          proximity:this.useLocation?.join(',')
        }
@@ -70,6 +72,8 @@ export class PlacesService {
        
        this.isLoadingPlaces=false;
        this.places= resp.features;
+
+       this.mapService.createMarkersFromPlaces(this.places)
      })
      
    }
