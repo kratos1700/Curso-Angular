8ae5393870ea74d731047e1359def0c68e10a7be
Kratos1700 <hunterstarsa@hotmail.com>
2024-09-21 00:31:51 +0200

    seccion 4 del curso, Bases

---
+++ 01-bases/src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+  public name:string = 'ironman';
+  public age:number = 45;
+
+
+get capitalizeName():string{
+  return this.name.toLocaleUpperCase();
+}
+getHeroDescription():string{
+  return `${this.name } - ${this.age}`;
+}
+
+}
