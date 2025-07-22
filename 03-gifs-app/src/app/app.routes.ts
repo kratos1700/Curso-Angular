d693547872e7e24ebbbc9ddb35999cb5b748db30
Kratos1700 <hunterstarsa@hotmail.com>
2025-07-22 23:03:35 +0200

    feat: implement dashboard, trending, and search pages with routing and layout

--- 03-gifs-app/src/app/app.routes.ts
+++ 03-gifs-app/src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // definim les rutes de l'aplicació
+  {
+    path: 'dashboard',
+    // carreguem el component de Dashboard de manera lazy
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component'),
+    // definim les rutes filles per a les pàgines de gifs
+    // aquestes rutes es carregaran dins del component de Dashboard
+    // i es mostraran en el seu interior
+    children: [
+
+      {
+        path: 'trending',
+        loadComponent: () => import('./gifs/pages/trending-page/trending-page.component'),
+      },
+
+      {
+        path: 'search',
+        loadComponent: () => import('./gifs/pages/search-page/search-page.component'),
+      },
+      {
+        path: '**',
+        redirectTo: 'trending',
+      }
+    ]
+
+
+  },
+
+
+
+
+
+
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  }
+
+];
